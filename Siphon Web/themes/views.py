717ffb203ff2f6234ea4aa95b3067b44c496e623
Michael Muratov <michael.muratov@mail.utoronto.ca>
2021-12-14 11:48:41 -0500

    initial Linear Network

--- Siphon Web/themes/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def binary(request):
-    return render(request, "post.html")
-    #return HttpResponse('Hello World')
